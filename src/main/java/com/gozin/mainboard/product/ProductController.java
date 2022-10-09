2e3557f925e96e13dd7aed80fe9a9be1ea3878ed
jihi32123 <jihi32123@gmail.com>
2022-10-09 15:51:06 +0900

    Add Product Api test

---
+++ src/main/java/com/gozin/mainboard/product/ProductController.java
@@ -0,0 +1,15 @@
+package com.gozin.mainboard.product;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @GetMapping
+    public String test(){
+        return "hello";
+    }
+}
