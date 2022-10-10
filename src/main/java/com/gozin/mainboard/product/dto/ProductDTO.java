1a3ea8d03e0944a187fee7915125f8095b1c96f5
jihi32123 <jihi32123@gmail.com>
2022-10-10 11:21:10 +0900

    Add grade status

--- src/main/java/com/gozin/mainboard/product/dto/ProductDTO.java
+++ src/main/java/com/gozin/mainboard/product/dto/ProductDTO.java
@@ -5,21 +5,35 @@ public class ProductDTO {
     private String boardgameName;
     private String soldOutYn;
     private int totalCount;
-    private int deffaultrentalFee;
+    private int defaultRentalFee;
     private String manufacturingCompany;
     private String brand;
     private String categoryName;
+    private int SRentalFee;
+    private int ARentalFee;
+    private int BRentalFee;
+    private String SState;
+    private String AState;
+    private String BState;
     public ProductDTO(){}
-    public ProductDTO(int boardgameType, String boardgameName, String soldOutYn, int totalCount, int deffaultrentalFee, String manufacturingCompany,
-                      String brand, String categoryName) {
+
+    public ProductDTO(int boardgameType, String boardgameName, String soldOutYn, int totalCount, int defaultRentalFee,
+                      String manufacturingCompany, String brand, String categoryName, int SRentalFee, int ARentalFee,
+                      int BRentalFee, String SState, String AState, String BState) {
         this.boardgameType = boardgameType;
         this.boardgameName = boardgameName;
         this.soldOutYn = soldOutYn;
         this.totalCount = totalCount;
-        this.deffaultrentalFee = deffaultrentalFee;
+        this.defaultRentalFee = defaultRentalFee;
         this.manufacturingCompany = manufacturingCompany;
         this.brand = brand;
         this.categoryName = categoryName;
+        this.SRentalFee = SRentalFee;
+        this.ARentalFee = ARentalFee;
+        this.BRentalFee = BRentalFee;
+        this.SState = SState;
+        this.AState = AState;
+        this.BState = BState;
     }
 
     @Override
@@ -29,10 +43,16 @@ public class ProductDTO {
                 ", boardgameName='" + boardgameName + '\'' +
                 ", soldOutYn='" + soldOutYn + '\'' +
                 ", totalCount=" + totalCount +
-                ", deffaultrentalFee=" + deffaultrentalFee +
+                ", defaultRentalFee=" + defaultRentalFee +
                 ", manufacturingCompany='" + manufacturingCompany + '\'' +
                 ", brand='" + brand + '\'' +
                 ", categoryName='" + categoryName + '\'' +
+                ", SRentalFee=" + SRentalFee +
+                ", ARentalFee=" + ARentalFee +
+                ", BRentalFee=" + BRentalFee +
+                ", SState='" + SState + '\'' +
+                ", AState='" + AState + '\'' +
+                ", BState='" + BState + '\'' +
                 '}';
     }
 
@@ -68,12 +88,12 @@ public class ProductDTO {
         this.totalCount = totalCount;
     }
 
-    public int getDeffaultrentalFee() {
-        return deffaultrentalFee;
+    public int getdefaultRentalFee() {
+        return defaultRentalFee;
     }
 
-    public void setDeffaultrentalFee(int deffaultrentalFee) {
-        this.deffaultrentalFee = deffaultrentalFee;
+    public void setdefaultRentalFee(int defaultRentalFee) {
+        this.defaultRentalFee = defaultRentalFee;
     }
 
     public String getManufacturingCompany() {
@@ -98,5 +118,53 @@ public class ProductDTO {
 
     public void setCategoryName(String categoryName) {
         this.categoryName = categoryName;
+    }
+
+    public int getSRentalFee() {
+        return SRentalFee;
+    }
+
+    public void setSRentalFee(int SRentalFee) {
+        this.SRentalFee = SRentalFee;
+    }
+
+    public int getARentalFee() {
+        return ARentalFee;
+    }
+
+    public void setARentalFee(int ARentalFee) {
+        this.ARentalFee = ARentalFee;
+    }
+
+    public int getBRentalFee() {
+        return BRentalFee;
+    }
+
+    public void setBRentalFee(int BRentalFee) {
+        this.BRentalFee = BRentalFee;
+    }
+
+    public String getSState() {
+        return SState;
+    }
+
+    public void setSState(String SState) {
+        this.SState = SState;
+    }
+
+    public String getAState() {
+        return AState;
+    }
+
+    public void setAState(String AState) {
+        this.AState = AState;
+    }
+
+    public String getBState() {
+        return BState;
+    }
+
+    public void setBState(String BState) {
+        this.BState = BState;
     }
 }
